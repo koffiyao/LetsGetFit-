a0105de7c9736b9d7f363523c65b2461a2e65b0d
koffiyao <koffi.yao@ttu.edu>
2017-04-26 17:46:51 -0500

    Formatted Json response in Nutrition activity.

--- app/src/main/java/com/ykeocorp/letsgetfit/NutritionActivity.java
+++ app/src/main/java/com/ykeocorp/letsgetfit/NutritionActivity.java
@@ -1,5 +1,6 @@
 package com.ykeocorp.letsgetfit;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,12 +11,9 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -24,90 +22,14 @@ import java.net.URL;
 
 public class NutritionActivity extends AppCompatActivity {
 
-    EditText foodText;
-    TextView responseView;
-    ProgressBar progressBar;
-
-    static final String APP_ID = "65009ff2";
-    static final String APP_KEY = "28a51cd162c6898cb107d06b74ef1bcc";
-    static final String API_URL_SEARCH = "https://api.nutritionix.com/v1_1/search/";
-    static final String API_URL_ITEM = "https://api.nutritionix.com/v1_1/item?";
-    static final String FIELD = "?results=0%3A10&" +
-            "fields=fields=item_name%2Cbrand_name%2Cnf_calories" +
-            "%2Cnf_total_fat%2Cnf_total_carbohydrate%2Cnf_protein&";
-
-    String resultJson;
-    Result[] resultArray;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrition);
-
-        foodText = (EditText) findViewById(R.id.editText);
-        responseView = (TextView) findViewById(R.id.responseView);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        Button queryButton = (Button) findViewById(R.id.button);
-        queryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new RetrieveFeedTask().execute();
-            }
-        });
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
-
-        private Exception exception;
-
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-            responseView.setText("");
-        }
-
-        protected String doInBackground(Void... urls) {
-            String food = foodText.getText().toString();
-            // Do some validation here
-
-            try {
-
-                URL url = new URL(API_URL_SEARCH + food + FIELD +
-                        "&appId=" + APP_ID + "&appKey=" + APP_KEY);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-
-                   /* resultJson = stringBuilder.toString();
-                    Gson gson = new Gson();
-                    resultArray = gson.fromJson(resultJson, Result[].class);*/
-                    return stringBuilder.toString();
-                } finally {
-                    urlConnection.disconnect();
-                }
-            } catch (Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            if (response == null) {
-                response = "THERE WAS AN ERROR";
-            }
-            progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
-            //response = resultArray;
-            responseView.setText(response);
-            // TODO: check this.exception
-            // TODO: do something with the feed
-
-
-        }
+    public void openLookFood(View view) {
+        Intent intent = new Intent(this, LookFoodActivity.class);
+        startActivity(intent);
     }
 }
