c3b12f2271745b6769c257af9282bfa9307c66b3
Ama Ott <amaott13@gmail.com>
2017-05-03 15:49:09 -0500

    Added notification to the Settings Page. User get notification when they turn on notification. Added notification icon.

--- app/src/main/java/com/ykeocorp/letsgetfit/SettingsActivity.java
+++ app/src/main/java/com/ykeocorp/letsgetfit/SettingsActivity.java
@@ -1,7 +1,17 @@
 package com.ykeocorp.letsgetfit;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 /**
  * Created by Ama on 4/18/2017.
@@ -10,7 +20,42 @@ import android.support.v7.app.AppCompatActivity;
 public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Switch s;
+        final View v;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        v = this.findViewById(android.R.id.content);
+
+        s = (Switch) findViewById(R.id.notificationSwitch);
+        //s.setChecked(true);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    sendNotification(v);
+            }
+        });
     }
+
+    public void sendNotification(View view) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+        //Create the intent that’ll fire when the user taps the notification//
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.androidauthority.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.drawable.notification_icon);
+        mBuilder.setContentTitle("Let's Get Fit!");
+        mBuilder.setContentText("Time for Your Work Out!");
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(001, mBuilder.build());
+    }
+
 }
